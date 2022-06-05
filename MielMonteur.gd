cd717ed93fcb2dbd043b04a77a7394a8770a7654
woutv <woutvelthuis01@gmail.com>
2022-06-05 16:13:07 +0200

    alles gefixt

--- MielMonteur.gd
+++ MielMonteur.gd
@@ -1,65 +1,58 @@
 extends KinematicBody2D
 
-signal hit
 
+const GRAVITY = 600
+const WALK_SPEED = 350
+const JUMP_FORCE = 500
 
-export var speed = 400
-export var gravity = 100
+var velocity = Vector2()
 var screen_size
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	hide()
-	screen_size = get_viewport_rect().size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+
 func _physics_process(delta):
-	var vel = Vector2()
 	var idle = true
-	vel.x = 0
-	vel.y = gravity * delta
-	if Input.is_action_pressed("move_right"):
-		vel.x += 1*speed*delta
-		idle = false
-	if Input.is_action_pressed("move_left"):
-		vel.x -= 1*speed*delta
-		idle = false
-	if Input.is_action_pressed("move_down"):
-		vel.y += 1*speed*delta
-		idle = false
-	if Input.is_action_pressed("move_up"):
-		vel.y -= 1*speed*delta
-		idle = false
-		
+	velocity.y += delta * GRAVITY
+	
 	$AnimatedSprite.play()
-		
-#	if vel.length() > 0:
-#		$AnimatedSprite.play()
-#	else:
-#		$AnimatedSprite.stop()
 	
-#	position += velocity * delta
-#	position.x = clamp(position.x, 0, screen_size.x)
-#	position.y = clamp(position.y, 0, screen_size.y)
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+		idle = false
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = WALK_SPEED
+		idle = false
+	else:
+		velocity.x = 0
+		# smoothen the stop
+		# velocity.x = lerp(velocity.x, 0, 0.1)
 	
-	if vel.x != 0:
+	if Input.is_action_pressed("ui_up") and is_on_floor():
+		velocity.y = -JUMP_FORCE
+		idle = false
+	 
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	# prevent player going out of screen
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.y != 0:
+		if velocity.y > 0:
+			$AnimatedSprite.animation = "fall"
+		else:
+			$AnimatedSprite.animation = "up"
+	elif velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		# See the note below about boolean assignment.
-		$AnimatedSprite.flip_h = vel.x < 0
-	elif vel.y != 0:
-		$AnimatedSprite.animation = "up"
-#		if vel.y > 0:
-#			$AnimatedSprite.stop()
+		$AnimatedSprite.flip_h = velocity.x < 0
 	if idle:
-#		$AnimatedSprite.play()
 		$AnimatedSprite.animation = "idle"
-		
-	var coll = move_and_collide(vel)
 	
-	if coll != null:
-#		$AnimatedSprite.animation = "walk"
-		move_local_x(vel.x)
-		move_local_y(vel.y)
+	print(velocity)
 	
 func start(pos):
 	position = pos
